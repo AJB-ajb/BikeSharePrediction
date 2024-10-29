bf64b7ba90eb8e3f6419274bf0abdf57ab284366
AJB-ajb <al.busch@accc.de>
2024-10-28 23:12:55 -0400

    add visualizations; update solutions; add jupyter notebook

--- src/analysis.py
+++ src/analysis.py
@@ -10,7 +10,10 @@ import math
 from scipy.ndimage import gaussian_filter1d
 
 def preprocess_ridership_table(df):
-    df = df[df["Trip  Duration"] != 0]
+    print("Preprocessing ridership table")
+
+#    df = df[df["Trip  Duration"] != 0]
+    print(f"Number of rows with NaN End Station Id: {df['End Station Id'].isna().sum()}")
     df = df[df["End Station Id"].notna()]
     df['End Station Id'] = df['End Station Id'].astype(int)
     df['Start Time'] = pd.to_datetime(df['Start Time'], format='%m/%d/%Y %H:%M')
@@ -27,52 +30,20 @@ ridership_table = pd.read_csv("../data/bikeshare-ridership-2024/Bike share rider
 def parse_time(time_str):
     return pd.to_datetime(time_str, format='%m/%d/%Y %H:%M')
 
-#initial_time = parse_time(ridership_table['Start Time'].iloc[0])
-
-# model a sequence, where the value changes infrequently
-class SparseSequence:
-    def __init__(self, events):
-        self.events = events # list of (time, new value)
-
-    def __getitem__(self, index):
-        return self.events[index]
-    
-    def get_value(self, t):
-        # find the most recent event that is <= t
-        index = bisect.bisect_right(self.events, t, key=lambda ev: ev[0]) - 1
-        return self.events[index] if index >= 0 else None
-    def set(self, t, value):
-        index = bisect.bisect_right(self.events, t, key=lambda ev: ev[0])
-        if index > 0 and self.events[index-1] == value: # if there is an event at this time, update it
-            self.events[index-1] = (t, value)
-        else:
-            self.events.insert(index, (t, value))
-    def __len__(self):
-        return len(self.events)
-    def __repr__(self):
-        return f'SparseSequence({self.events})'
-    
-    
-
-
-# data model
-@dataclass
-class BikeStation:
-    station_id: int
-    name: str
-    lat: float = float('NaN')
-    lon: float = float('NaN')
-    capacity: int = -1
-    num_bikes_t0: int = 0
-    num_bikes: SparseSequence = SparseSequence([])
-
-    def __post_init__(self):
-        self.num_bikes = SparseSequence([])
 
 class BikeShareData:
     def __init__(self, name):
-        self.stations = dict() # station_id -> BikeStation
         self.name = name
+
+    def process_data_and_save(self, ridership_table):
+        self.load_current_stations()
+        ridership_table = preprocess_ridership_table(ridership_table)
+        self._process_data_by_minute(ridership_table)
+        self.calculate_in_out_rates()
+        self.calc_capacities()
+        #self.calc_graph()
+        self.save_pickle()
+        return ridership_table
 
     def _process_data_by_minute(self, ridership_table):
         """
@@ -85,17 +56,16 @@ class BikeShareData:
         """
         df = ridership_table
 
-        start_time = df['Start Time'].min()
+        start_time = df['Start Time'].min().replace(second=0, microsecond=0, minute=0)
         end_time = df['End Time'].max()
         num_minutes = math.ceil((end_time - start_time).total_seconds() / 60) + 1
         # Here, we assume that the station ids are contiguous integers starting from 0
         # In practise, some stations are removed, but these then appear as pure 0 rows in the data
         # Here, take the number of stations from the table, which might differ from month to month; if merging data from multiple months, take the maximum 
-        num_stations = max(df['Start Station Id'].nunique(), df['End Station Id'].nunique())
-
-        station_id0 = min(df['Start Station Id'].min(), df['End Station Id'].min())
-        station_id_end = max(df['Start Station Id'].max(), df['End Station Id'].max())
-        num_stations = station_id_end - station_id0 + 1
+        # num_stations = max(df['Start Station Id'].nunique(), df['End Station Id'].nunique())  # old
+        # Here, use the stations from the current json data which gives some more stations than included but allows merging all months
+        num_stations = len(self.stations)
+        station_id0 = 7000
 
         start_dt_in_minutes = ((df['Start Time'] - start_time).dt.total_seconds() / 60).astype(np.int32)
         end_dt_in_minutes = ((df['End Time'] - start_time).dt.total_seconds() / 60).astype(np.int32)
@@ -105,7 +75,7 @@ class BikeShareData:
         df2['End Time'] = end_dt_in_minutes
 
 
-        in_bikes, out_bikes = np.zeros((num_stations, num_minutes), dtype=np.float32), np.zeros((num_stations, num_minutes), dtype=np.float32) # for calculations later, we use float anyway
+        in_bikes, out_bikes = np.zeros((num_stations, num_minutes), dtype=np.int16), np.zeros((num_stations, num_minutes), dtype=np.int16)
 
         # group every ride by start station and start time and then count the number of rides out from each station in each minute
         # same for the end stations
@@ -128,50 +98,44 @@ class BikeShareData:
         """
         # todo: find a suitable σ, such that the rates are smooth enough to reflect demand patterns, but local enough to capture important patterns
         # probably between 5 and 60 minutes
-        in_rates = gaussian_filter1d(self.in_bikes, sigma=σ_minutes, axis=1, mode='reflect') # / σ_minutes
-        out_rates = gaussian_filter1d(self.out_bikes, sigma=σ_minutes, axis=1, mode='reflect') # / σ_minutes
 
-        return in_rates, out_rates
+        # convert types to float32 to calculate moving average
+        in_bikes = self.in_bikes.astype(np.float32)
+        out_bikes = self.out_bikes.astype(np.float32)
+        
+        self.in_rates = gaussian_filter1d(in_bikes, sigma=σ_minutes, axis=1, mode='reflect') # / σ_minutes
+        self.out_rates = gaussian_filter1d(out_bikes, sigma=σ_minutes, axis=1, mode='reflect') # / σ_minutes
+        self.σ_minutes = σ_minutes
 
-
+        return self.in_rates, self.out_rates
 
 
     def load_current_stations(self):
         r = requests.get('https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information')
 
-        bikeshare_stations = pd.DataFrame(json.loads(r.content)['data']['stations'])[['station_id', 'name', 'lat', 'lon']].astype({
+        self.stations = pd.DataFrame(json.loads(r.content)['data']['stations'])[['station_id', 'name', 'lat', 'lon', 'capacity', 'altitude', 'is_charging_station']].astype({
           'station_id': 'int',
             'name': 'string',
         })
-        for i_row, row in bikeshare_stations.iterrows():
-            station_id = row['station_id']
-            if station_id not in self.stations:
-                self.stations[station_id] = BikeStation(station_id, row['name'], row['lat'], row['lon'])
-            else: # update the lat, lon, name
-                self.stations[station_id].lat = row['lat']
-                self.stations[station_id].lon = row['lon']
-                self.stations[station_id].name = row['name']
-            
+        self.stations.sort_values('station_id', inplace=True)
+        min_station_id = self.stations['station_id'].min()
+        max_station_id = self.stations['station_id'].max()
 
-    def load_data(self, ridership_table):
-        initial_time = parse_time(ridership_table['Start Time'].iloc[0])
-        for i_row, row in ridership_table.iterrows():
-            start_station_id = int(row['Start Station Id'])
-            end_station_id = int(row['End Station Id'])
-            
-            if start_station_id not in self.stations:
-                self.stations[start_station_id] = BikeStation(start_station_id, row['Start Station Name'])
+        all_station_ids = set(range(min_station_id, max_station_id + 1))
+        existing_station_ids = set(self.stations['station_id'])
+        missing_station_ids = all_station_ids - existing_station_ids
 
-            if end_station_id not in self.stations:
-                self.stations[end_station_id] = BikeStation(end_station_id, row['End Station Name'])
-            
-            start_station = self.stations[start_station_id]
-            start_time = parse_time(row['Start Time'])
-            start_station.num_bikes.set(start_time, start_station.num_bikes.get_value(start_time)[1] - 1)
+        missing_stations = pd.DataFrame({
+            'station_id': list(missing_station_ids),
+            'name': ['Unknown'] * len(missing_station_ids),
+            'lat': [np.nan] * len(missing_station_ids),
+            'lon': [np.nan] * len(missing_station_ids),
+            'capacity': [0] * len(missing_station_ids)
+        })
 
-            end_time = parse_time(row['End Time'])
-            end_station = self.stations[end_station_id]
-            end_station.num_bikes.set(end_time, end_station.num_bikes.get_value(end_time)[1] + 1)
+        self.stations = pd.concat([self.stations, missing_stations]).sort_values('station_id').reset_index(drop=True)
+        print(f'Loaded {len(self.stations)} stations')
+    
 
     def save_pickle(self, file_path = None):
             if file_path is None:
@@ -186,13 +150,18 @@ class BikeShareData:
             return pickle.load(open(f'../data/{name}.pkl', 'rb'))
     
     def calc_capacities(self):
-        for station in self.stations.values():
-            # we assume here that the capacity of every station is fully utilized at some point in the data and the bikes are empty at some point
-            bike_max = np.max([num_bikes for _, num_bikes in station.num_bikes.events])
-            bike_min = np.min([num_bikes for _, num_bikes in station.num_bikes.events])
-            station.capacity = bike_max - bike_min
-            station.num_bikes_t0 = -bike_min
-            station.num_bikes.events = SparseSequence([(time, bikes + station.num_bikes_t0) for (time, bikes) in station.num_bikes.events])
+        # we assume here that the capacity of every station is fully utilized frequently at some point in the data and the bikes are empty at some point
+        # this should be true for most stations that are frequently used
+        # We assume that the capacity stays constant over the month
+        # We estimate the capacity as the median of ((max_day - min_day)_day for day in month)
+        
+        cum_in = np.cumsum(self.in_bikes, axis=1)
+        cum_out = np.cumsum(self.out_bikes, axis=1)
+        Δbikes = cum_in - cum_out # the additional number of bikes relative to the number of bikes at the start of the data
+        self.capacities = np.max(Δbikes, axis=1) - np.min(Δbikes, axis=1)
+        self.N_bikes = Δbikes - np.min(Δbikes, axis=1)[:, None] # the number of bikes at each station
+
+            
     def calc_graph(self):
         # Create a directed graph
         G = nx.DiGraph()
